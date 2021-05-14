658c06d9285dcec2add548ff475b3717c7915cfa
Jose <josephgaspar725@gmail.com>
2021-05-15 09:58:41 +1000

    First commit. Application finished.

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const notes = require("./notes/functions");
+const bodyParser = require("body-parser");
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(
+  express.static("public", {
+    extensions: ["html"],
+  })
+);
+
+app.get("/api/notes", async (req, res) => {
+  const data = await notes.read(notes);
+  res.json(data);
+});
+
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+  const note = {
+    title,
+    text,
+  };
+  notes.write(note);
+  res.json(notes);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const { id } = req.params;
+  notes.remove(id);
+  res.json({ id });
+});
+
+const port = 80;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
---
+++ server/notes/functions.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+
+function read () {
+    return new Promise((resolve, reject) => {
+        // Read file
+        fs.readFile('./notes.json', 'utf-8', function(err, data) {
+            console.log('data', data);
+            resolve(JSON.parse(data));
+        })   
+    });
+}
+
+function write (newNote) {
+    return new Promise(async (resolve, reject) => {
+        // Get all notes
+        const notes = await read();
+
+        // Add note id
+        const [lastNote] = notes.slice(-1);
+        newNote.id = String(Number(lastNote.id) + 1);
+
+        // Push note into notes list
+        notes.push(newNote);
+        
+        // Update file
+        updateNotes(notes);
+    });
+}
+
+async function remove(noteID) {
+    // Get all notes
+    const notes = await read();
+
+    // Filter the note that should be removed
+    const newNotes = notes.filter(note => note.id !== noteID);
+
+    // Update note list without the removed one
+    updateNotes(newNotes, () => console.log('Note removed!'));
+}
+
+function updateNotes(notes, callback) {
+    fs.writeFile('./notes.json', JSON.stringify(notes), function(err, data) {
+        console.log(data);
+
+        if (callback) {
+            callback();
+        }
+    });
+}
+
+module.exports = {
+    read,
+    write,
+    remove
+};
